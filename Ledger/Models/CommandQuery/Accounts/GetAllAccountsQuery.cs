04bb494c0e5763b15790f71503afb5b259409867
mgroves <me@mgroves.com>
2014-07-10 08:04:10 -0400

    order accounts by name

--- Ledger/Models/CommandQuery/Accounts/GetAllAccountsQuery.cs
+++ Ledger/Models/CommandQuery/Accounts/GetAllAccountsQuery.cs
@@ -10,7 +10,7 @@ namespace Ledger.Models.CommandQuery.Accounts
     {
         public List<Account> Execute(IDbConnection db)
         {
-            return db.Query<Account>("SELECT id, desc, category, comment FROM accounts").ToList();
+            return db.Query<Account>("SELECT id, desc, category, comment FROM accounts ORDER BY desc").ToList();
         }
     }
 }
